459ad273b6ecf221a5f0417f55db81c640e82b3f
Aquiles <aquiles2ws@gmail.com>
2025-03-14 11:20:00 -0300

    ft: criacão da tabela 'pedidos' e inserção de valores.

--- datasets/dataset.sql
+++ datasets/dataset.sql
@@ -5,6 +5,24 @@ CREATE TABLE Clientes (
     email VARCHAR(100) UNIQUE
 );
 
+-- Tabela produtos
+CREATE TABLE Produtos (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DECIMAL(10, 2) 
+);
+
+-- Tabela pedidos
+CREATE TABLE Pedidos (
+    id_pedido SERIAL PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    data_pedido DATE NOT NULL
+    FOREIGN KEY(id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY(id_produto) REFERENCES Produtos(id_produto),
+);
+
 INSERT INTO Clientes (nome, email)
 VALUES
 ('João Silva', 'joao@email.com'),
@@ -12,15 +30,16 @@ VALUES
 ('Carlos Santos', 'carlos@email.com'),
 ('Ana Souza', 'ana@email.com');
 
-CREATE TABLE Produtos (
-    id_produto SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    preco DECIMAL(10, 2) 
-);
-
 INSERT INTO Produtos (nome, preco)
 VALUES
-('Notebook', 3500.00)
-('Smartphone', 2200.00)
-('Mouse', 120.00)
-('Teclado', 200.00)
+('Notebook', 3500.00),
+('Smartphone', 2200.00),
+('Mouse', 120.00),
+('Teclado', 200.00);
+
+INSERT INTO Pedidos (id_cliente, id_produto, quantidade, data_pedido)
+VALUES
+(1, 1, 1, '2024-03-01'),
+(1, 3, 2, '2024-03-02'),
+(2, 2, 1, '2024-03-03'),
+(3, 4, 1, '2024-03-04');
