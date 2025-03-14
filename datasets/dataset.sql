ae9560d24961bf5ca41fd838313f8d550a6a35c8
Aquiles <aquiles2ws@gmail.com>
2025-03-14 11:04:32 -0300

    ft: criacão da tabela 'produtos' e inserção de valores.

--- datasets/dataset.sql
+++ datasets/dataset.sql
@@ -1,13 +1,26 @@
 -- Tabela clientes
-CREATE TABLE clientes (
+CREATE TABLE Clientes (
     id_cliente SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE
 );
 
-INSERT INTO clientes (nome, email)
+INSERT INTO Clientes (nome, email)
 VALUES
 ('João Silva', 'joao@email.com'),
 ('Maria Oliveira ', 'maria@email.com'),
 ('Carlos Santos', 'carlos@email.com'),
 ('Ana Souza', 'ana@email.com');
+
+CREATE TABLE Produtos (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DECIMAL(10, 2) 
+);
+
+INSERT INTO Produtos (nome, preco)
+VALUES
+('Notebook', 3500.00)
+('Smartphone', 2200.00)
+('Mouse', 120.00)
+('Teclado', 200.00)
