2db140fd1a4c49d8181d55b130242ac24148f43d
Aquiles <aquiles2ws@gmail.com>
2025-03-14 10:52:05 -0300

    ft: inserção de valores na tabela 'clientes'

--- datasets/dataset.sql
+++ datasets/dataset.sql
@@ -1,5 +1,13 @@
 -- Tabela clientes
 CREATE TABLE clientes (
     id_cliente SERIAL PRIMARY KEY,
-    
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
+
+INSERT INTO clientes (nome, email)
+VALUES
+('João Silva', 'joao@email.com'),
+('Maria Oliveira ', 'maria@email.com'),
+('Carlos Santos', 'carlos@email.com'),
+('Ana Souza', 'ana@email.com');
